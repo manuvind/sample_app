09dfd56925bcfbedbd07d124e7741562aad84b16
Anuvind Venu Menon <manuvind@gmail.com>
2013-01-01 17:34:28 -0700

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -51,6 +51,8 @@ describe "User pages" do
   end
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
     before do
       sign_in user
@@ -59,6 +61,12 @@ describe "User pages" do
 
     it { should have_selector('h1',     text: user.name) }
     it { should have_selector('title',  text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup page" do
